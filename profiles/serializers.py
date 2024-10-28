98ef1cf563befcf46c58d0b0decae1aa17f4249a
Kit Walker <Neocoinkitt@gmail.com>
2024-10-28 14:52:16 +0000

    fixed and tested some things

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,21 +2,16 @@ from rest_framework import serializers
 from .models import Profile
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email']
-
 class ProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+    user = serializers.ReadOnlyField(source='user.username')
     running_level_display = serializers.CharField(source='get_running_level_display', read_only=True)
+    is_owner = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
-        fields = ['id', 'user', 'bio', 'location', 'running_level', 'running_level_display', 'image', 'created_at', 'updated_at']
+        fields = ['id', 'user', 'bio', 'location', 'running_level', 'running_level_display', 'image', 'created_at', 'updated_at', 'is_owner']
         read_only_fields = ['id', 'user', 'created_at', 'updated_at']
 
-class ProfileCreateUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ['bio', 'location', 'running_level', 'image']
+    def get_is_owner(self, obj):
+        request = self.context.get('request')
+        return request.user == obj.user if request and request.user.is_authenticated else False
--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,19 @@
 from rest_framework import viewsets, permissions
 from .models import Profile
-from .serializers import ProfileSerializer, ProfileCreateUpdateSerializer
+from .serializers import ProfileSerializer
 from epona_drf_api.permissions import IsOwnerOrReadOnly
 
 class ProfileViewSet(viewsets.ModelViewSet):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.all().order_by('-created_at')
+    serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-
-    def get_serializer_class(self):
-        if self.action in ['create', 'update', 'partial_update']:
-            return ProfileCreateUpdateSerializer
-        return ProfileSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        queryset = Profile.objects.all()
+        username = self.request.query_params.get('username', None)
+        if username is not None:
+            queryset = queryset.filter(user__username=username)
+        return queryset.order_by('-created_at')
