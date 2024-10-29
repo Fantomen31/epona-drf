6bae030a94c9b4a12c85b36eb1ce5d270fd57406
Kit Walker <Neocoinkitt@gmail.com>
2024-10-29 15:07:32 +0000

    updated profile serializers to see participated events

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,18 +1,20 @@
 from rest_framework import serializers
 from .models import Profile
 from django.contrib.auth.models import User
+from events.serializers import EventSerializer
 
 class ProfileSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
     running_level_display = serializers.CharField(source='get_running_level_display', read_only=True)
     is_owner = serializers.SerializerMethodField()
+    participated_events = EventSerializer(many=True, read_only=True)
 
     class Meta:
         model = Profile
         fields = [
             'id', 'user', 'bio', 'location', 'running_level', 
             'running_level_display', 'image', 'created_at', 
-            'updated_at', 'is_owner'
+            'updated_at', 'is_owner', 'participated_events'
         ]
         read_only_fields = ['id', 'user', 'created_at', 'updated_at']
 
