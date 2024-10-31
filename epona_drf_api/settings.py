8a2d6d597b13a6ef237a917e1250d45f3e65bcb8
Kit Walker <Neocoinkitt@gmail.com>
2024-10-31 17:01:55 +0000

    trying to fix deployment db issue

--- epona_drf_api/settings.py
+++ epona_drf_api/settings.py
@@ -174,6 +174,14 @@ WSGI_APPLICATION = 'epona_drf_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+DATABASES = {
+    'default': ({
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    } if 'DEV' in os.environ else dj_database_url.parse(
+        os.environ.get('DATABASE_URL')
+    ))
+}
 
 DATABASES = {
     'default': dj_database_url.config(default=os.environ['DATABASE_URL'], 
