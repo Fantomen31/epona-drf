2351f82da60a2e3fe577320802afd4ffbc123f0d
Kit Walker <Neocoinkitt@gmail.com>
2024-11-05 12:42:39 +0000

    updated cors allowed orgins regexes wit hcodeisntitute-ide

--- epona_drf_api/settings.py
+++ epona_drf_api/settings.py
@@ -139,7 +139,7 @@ if 'CLIENT_ORIGIN' in os.environ:
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        r"^https://.*\.codeinstitute-ide\.net$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
