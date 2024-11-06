b12a33eb8e9a675211da5d5954ce32241b811142
Kit Walker <Neocoinkitt@gmail.com>
2024-11-06 12:49:40 +0000

    created shell for featured content on the home page

--- frontend/src/components/FeaturedCarousel.js
+++ frontend/src/components/FeaturedCarousel.js
@@ -1,32 +1,50 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import FeaturedCard from './FeaturedCard';
 import styles from '../styles/FeaturedCarousel.module.css';
 
-const FeaturedCarousel = ({ isLoggedIn }) => {
-  const featuredItems = [
-    { id: 1, title: 'San Francisco', type: 'City', image: '/placeholder.svg?height=400&width=800' },
-    { id: 2, title: 'Morning Runners Club', type: 'Club', image: '/placeholder.svg?height=400&width=800' },
-    { id: 3, title: 'Bay to Breakers', type: 'Event', image: '/placeholder.svg?height=400&width=800' },
-  ];
+const placeholderData = [
+  {
+    type: 'City',
+    name: 'New York City',
+    image: '/placeholder.svg?height=400&width=800',
+    stat: '50,000 runners',
+  },
+  {
+    type: 'Club',
+    name: 'Central Park Runners',
+    image: '/placeholder.svg?height=400&width=800',
+    stat: '5,000 members',
+  },
+  {
+    type: 'Event',
+    name: 'NYC Marathon',
+    image: '/placeholder.svg?height=400&width=800',
+    stat: '50,000 participants',
+  },
+];
 
+const FeaturedCarousel = () => {
   return (
-    <Carousel className={styles.carousel}>
-      {featuredItems.map((item) => (
-        <Carousel.Item key={item.id} className={styles.carouselItem}>
-          <FeaturedCard
-            title={item.title}
-            type={item.type}
-            image={item.image}
-            isLoggedIn={isLoggedIn}
-          />
-          <Carousel.Caption className={styles.carouselCaption}>
-            <h3 className={styles.carouselTitle}>{item.title}</h3>
-            <p className={styles.carouselDescription}>Top {item.type}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <div className={styles.carouselContainer}>
+      <Carousel fade interval={5000} className={styles.carousel}>
+        {placeholderData.map((item, index) => (
+          <Carousel.Item key={index}>
+            <img
+              className={`d-block w-100 ${styles.carouselImage}`}
+              src={item.image}
+              alt={item.name}
+            />
+            <Carousel.Caption className={styles.carouselCaption}>
+              <h3 className={styles.captionTitle}>
+                {item.type} Spotlight
+              </h3>
+              <h4 className={styles.captionName}>{item.name}</h4>
+              <p className={styles.captionStat}>{item.stat}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
   );
 };
 
