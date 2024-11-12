3719acfa6fdfedaa219d48d1ccac7f448c53e0c4
Kit Walker <Neocoinkitt@gmail.com>
2024-11-12 11:11:17 +0000

    created clubsectiontitle component

---
+++ frontend/src/pages/club/ClubSectionTitle.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from '../../styles/ClubProfilePage.module.css'
+
+const ClubSectionTitle = ({ children }) => (
+  <h2 className={styles.sectionTitle}>{children}</h2>
+)
+
+export default ClubSectionTitle
