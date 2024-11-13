8ff6b3f7d24668e2f89738bf81b6632d6b470d83
Kit Walker <Neocoinkitt@gmail.com>
2024-11-13 08:46:21 +0000

    changed order of menu items in profilesidemenu

--- frontend/src/pages/profile/ProfileSideMenu.js
+++ frontend/src/pages/profile/ProfileSideMenu.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaRoute, FaTrophy, FaUsers, FaCity, FaCalendarAlt, FaRunning, FaUser } from 'react-icons/fa';
+import { FaRoute, FaTrophy, FaUsers, FaCity, FaRunning, FaUser } from 'react-icons/fa';
 import styles from '../../styles/ProfileSideMenu.module.css';
 
 const ProfileSideMenu = () => {
   return (
     <nav className={styles.sideMenu}>
       <ul className={styles.menuList}>
-        <li className={`${styles.menuItem} ${styles.disabled}`}>
-          <Link to="/routes">
-          <FaRoute className={styles.icon} />
-          <span>Routes</span>
-          </Link>
-        </li>
-        <li className={`${styles.menuItem} ${styles.disabled}`}>
-          <Link to="/routes">
-          <FaTrophy className={styles.icon} />
-          <span>Leaderboards</span>
+      <li className={styles.menuItem}>
+          <Link to="/profiles/:id">
+            <FaUser className={styles.icon} />
+            <span>Profile</span>
           </Link>
         </li>
         <li className={styles.menuItem}>
@@ -37,16 +31,16 @@ const ProfileSideMenu = () => {
             <span>Events</span>
           </Link>
         </li>
-        <li className={styles.menuItem}>
-          <Link to="/profiles/:id">
-            <FaUser className={styles.icon} />
-            <span>Profile</span>
+        <li className={`${styles.menuItem} ${styles.disabled}`}>
+          <Link to="/routes">
+          <FaRoute className={styles.icon} />
+          <span>Routes</span>
           </Link>
         </li>
-        <li className={styles.menuItem}>
-          <Link to="/calendar">
-            <FaCalendarAlt className={styles.icon} />
-            <span>Calendar</span>
+        <li className={`${styles.menuItem} ${styles.disabled}`}>
+          <Link to="/leaderboards">
+          <FaTrophy className={styles.icon} />
+          <span>Leaderboards</span>
           </Link>
         </li>
       </ul>
