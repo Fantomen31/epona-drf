b00af267f7097c1c85588295ed1f071ae02f84ea
Kit Walker <Neocoinkitt@gmail.com>
2024-11-24 15:15:36 +0000

    created a sepperate edit profile page

---
+++ frontend/src/pages/profile/EditProfilePage.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import EditProfileForm from './EditProfileForm';
+import { useUserProfile } from '../../hooks/useUserProfile';
+import styles from '../../styles/EditProfilePage.module.css';
+import ProfileSideMenu from './ProfileSideMenu';
+
+const EditProfilePage = () => {
+  const { userProfile, refreshUserProfile } = useUserProfile();
+  const navigate = useNavigate();
+
+  const handleCancel = () => {
+    navigate('/profile');
+  };
+
+  const handleProfileUpdated = (updatedProfile) => {
+    refreshUserProfile();
+    navigate('/profile');
+  };
+
+  if (!userProfile) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Container fluid>
+      <Row>
+        <Col md={3}>
+          <ProfileSideMenu />
+        </Col>
+        <Col md={9}>
+          <div className={styles.editProfilePageContent}>
+            <h1 className={styles.pageTitle}>Edit Profile</h1>
+            <EditProfileForm 
+              userProfile={userProfile}
+              onCancel={handleCancel}
+              onProfileUpdated={handleProfileUpdated}
+            />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default EditProfilePage;
